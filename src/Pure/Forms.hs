dfed604e938366fcb9a8a4f3e6f2837eee81abb4
grumply <sean@grump.ly>
2018-12-31 06:57:53 -0600

    Improve safety with newtype.

--- src/Pure/Forms.hs
+++ src/Pure/Forms.hs
@@ -1,10 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, NoDeriveAnyClass, CPP, AllowAmbiguousTypes,
              RankNTypes, ViewPatterns, TypeApplications, ScopedTypeVariables,
              MultiParamTypeClasses, FlexibleContexts #-}
-module Pure.Forms
-  ( module Pure.Forms
-  , (<$|), (=<|), (|>=), (=<||>), (=<||>=)
-  ) where
+module Pure.Forms where
 
 import Pure hiding (get)
 import Pure.Data.Txt.Trie as Trie
@@ -21,13 +18,14 @@ import Control.Monad.Trans.State (StateT,evalStateT)
 import Data.Maybe
 import Data.Typeable
 
-newtype FormStore ty = FormStore { unFormStore :: SRef (ty,TxtTrie Value) }
-newtype FormM ty m a = FormM { unFormM :: StateT (FormStore ty) (PureM (ty,TxtTrie Value) m) a }
+newtype FormState ty = FormState { unFormState :: (ty,TxtTrie Value) }
+newtype FormRef ty = FormRef { unFormRef :: SRef (FormState ty) }
+newtype FormM ty m a = FormM { unFormM :: StateT (FormRef ty) (PureM (FormState ty) m) a }
   deriving (Functor,Applicative,Monad,Alternative,MonadPlus)
 
 type FormIO ty a = FormM ty IO a
 
-instance Monad m => MonadState (FormStore ty) (FormM ty m) where
+instance Monad m => MonadState (FormRef ty) (FormM ty m) where
   {-# INLINE get #-}
   get = FormM get
   {-# INLINE put #-}
@@ -46,15 +44,15 @@ instance MonadFix m => MonadFix (FormM ty m) where
   mfix = FormM . mfix . (unFormM .)
 
 formWith :: forall ty m. (Monad m, Typeable ty, Typeable m) => ty -> (forall a. m a -> IO a) -> FormM ty m View -> View
-formWith ty g f = runPureWith g (ty,mempty) $ \(FormStore -> st) -> evalStateT (unFormM f) st
+formWith ty g f = runPureWith g (FormState (ty,mempty)) $ \(FormRef -> st) -> evalStateT (unFormM f) st
 
 form :: Typeable ty => ty -> FormIO ty View -> View
-form ty f = runPureWithIO (ty,mempty) $ \(FormStore -> st) -> evalStateT (unFormM f) st
+form ty f = runPureWithIO (FormState (ty,mempty)) $ \(FormRef -> st) -> evalStateT (unFormM f) st
 
 value :: forall a ty m. (MonadIO m, FromJSON a) => Txt -> FormM ty m (Maybe a)
 value k = do
-  FormStore ref <- get
-  (_,store) <- getWith ref
+  FormRef ref <- get
+  FormState (_,store) <- getWith ref
   case Trie.lookup k store of
     Just (fromJSON -> Success a) -> pure (Just a)
     _ -> pure Nothing
@@ -62,15 +60,15 @@ value k = do
 store :: forall a ty m. (MonadIO m, ToJSON a) => Txt -> a -> FormM ty m ()
 store k v = get >>= \st -> storeWith st k v
 
-valueWith :: forall a ty m. (MonadIO m, FromJSON a) => FormStore ty -> Txt -> m (Maybe a)
-valueWith (FormStore ref) f = do
-  (_,store) <- getWith ref
+valueWith :: forall a ty m. (MonadIO m, FromJSON a) => FormRef ty -> Txt -> m (Maybe a)
+valueWith (FormRef ref) f = do
+  FormState (_,store) <- getWith ref
   case Trie.lookup f store of
     Just (fromJSON -> Success a) -> pure (Just a)
     _ -> pure Nothing
 
-storeWith :: forall a ty m. (MonadIO m, ToJSON a) => FormStore ty -> Txt -> a -> m ()
-storeWith (FormStore ref) k v = modifyWith ref (fmap (Trie.insert k (toJSON v)))
+storeWith :: forall a ty m. (MonadIO m, ToJSON a) => FormRef ty -> Txt -> a -> m ()
+storeWith (FormRef ref) k v = modifyWith ref (FormState . fmap (Trie.insert k (toJSON v)) . unFormState)
 
 field :: forall ty a m. (MonadIO m, ToJSON a, FromJSON a)
       => Txt -> a -> ((a -> IO ()) -> FormM ty m View) -> FormM ty m (View,a)
